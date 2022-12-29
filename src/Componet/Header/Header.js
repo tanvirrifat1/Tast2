714a0be97ea54218c04c9ec7cbb5bbfadc453dd4
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-12-29 22:38:27 +0600

    header added

---
+++ src/Componet/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import image from '../../file/Group 1.png'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar text-white">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl"><img className='h-10 w-10 mr-2' src={image} alt="" /> SuperBio</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1 ">
+                        <li><a>Blog</a></li>
+                        <li className='text-blue-400'><a>LogIn</a></li>
+                        <li className='bg-blue-700 rounded-lg'><a>SignUp</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
